7fb6903d0a4fa1c7fba07a1ee080661a69b67697
Maren Christensen <marenjoychristensen@gmail.com>
2023-10-02 14:16:25 -0500

    adjusted paths in webpack config to troubleshoot.

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -7,28 +7,38 @@ module.exports = () => {
   return {
     mode: 'development',
     entry: {
-      main: '/develop/client/src/js/index.js',
-      install: '/develop/client/src/js/install.js',
+      main: './src/js/index.js',
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      //plugin to generate html file
+      new HtmlWebpackPlugin({
+        template: './client/src/index.html',
+        title: 'JATE',
+      }),
       new InjectManifest({
-        swSrc: '/develop/client/src/js/sw.js',
-        swDest: 'sw.js',
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       }),
       new WebpackPwaManifest({
+        inject: true,
         name: 'Just Another Text Editor',
         short_name: 'JATE',
         description: 'An application that allows you to input and edit text!',
         background_color: '#ffffff',
-        crossorigin: 'use-credentials', 
-        icons: [
+        crossorigin: 'use-credentials',
+        theme_color: '#ffffff',
+        start_url: '/',
+        publicPath: '/', 
+        images: [
           {
-            src: path.resolve('/develop/client/src/images/logo.png'),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'images'),
           },
         ],
       }),
@@ -47,6 +57,7 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              plugins: [['@babel/plugin-transform-runtime']],
             },
           },
         },
